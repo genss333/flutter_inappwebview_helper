1c28b7541dc67990e7d42bec9bebae156029a59f
Tusspon Moonsan <109135068+genss333@users.noreply.github.com>
2024-10-27 18:56:58 +0700

    Update flutter_inappwebview_helper package and dependencies

--- lib/flutter_inappwebview_helper.dart
+++ lib/flutter_inappwebview_helper.dart
@@ -1,3 +1,5 @@
+export 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
 export 'src/model.dart' show WebviewGETModel, WebviewPOSTModel;
 export 'src/repository.dart' show WebviewRepository;
 export 'src/service.dart' show WebViewService;
